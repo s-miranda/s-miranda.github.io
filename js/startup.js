f9b31b4ec6c158b4d1755fff1494042236ea5a5b
s-miranda <zak.ayles@gmail.com>
2020-06-20 07:59:25 -0500

    Update startup.js

--- js/startup.js
+++ js/startup.js
@@ -1,11 +1,13 @@
 $(function() {
-	$('#1').animate({top: "-6px", opacity: "0"}, 0);
-	$('#2').animate({top: "-6px", opacity: "0"}, 0);
-	$('#3').animate({top: "-6px", opacity: "0"}, 0);
+	if(window.location.href == "s-miranda.github.io"){
+		$('#1').animate({top: "-6px", opacity: "0"}, 0);
+		$('#2').animate({top: "-6px", opacity: "0"}, 0);
+		$('#3').animate({top: "-6px", opacity: "0"}, 0);
 
-	$('#icon').hide().delay(750).fadeIn(1000, function() {
-		$('#1').animate({top: "0px", opacity: "1"}, 500);
-		$('#2').delay(250).animate({top: "0px", opacity: "1"}, 500);
-		$('#3').delay(500).animate({top: "0px", opacity: "1"}, 500);
-	});
+		$('#icon').hide().delay(750).fadeIn(1000, function() {
+			$('#1').animate({top: "0px", opacity: "1"}, 500);
+			$('#2').delay(250).animate({top: "0px", opacity: "1"}, 500);
+			$('#3').delay(500).animate({top: "0px", opacity: "1"}, 500);
+		});
+	}
 });
